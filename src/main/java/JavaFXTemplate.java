29bce42edeec9291fe9b9b030f20849c2fee339f
kevin <kevin@kevinramirez.dev>
2021-10-18 14:46:56 -0500

    added the basics for game play screen

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -4,6 +4,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -26,7 +27,11 @@ public class JavaFXTemplate extends Application {
 
 	private Button startGameBtn, exitGameBtn;
 	private EventHandler<ActionEvent> closeHandler;
-	private GridPane gameBoard = new GridPane();
+	private GridPane gameBoard;
+	private BorderPane gamePane;
+	private Text whichPlayer;
+	private ListView<String> gameLog;
+	private GameButton [][] gameArray = new GameButton[ROWS][COLUMNS];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -37,6 +42,8 @@ public class JavaFXTemplate extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("Welcome to Connect Four!");
+
+		gamePane = new BorderPane();
 
 		// Button declarations
 		startGameBtn = new Button("Play");
@@ -54,6 +61,7 @@ public class JavaFXTemplate extends Application {
 
 		// Assign Handlers
 		exitGameBtn.setOnAction(closeHandler);
+		startGameBtn.setOnAction(e -> primaryStage.setScene(gamePlayScreen()));
 
 		// End Assign Handlers
 
@@ -78,13 +86,58 @@ public class JavaFXTemplate extends Application {
 		pane.setCenter(view);
 		pane.setBottom(hBox);
 
-		pane.setStyle("-fx-background-color: lightsalmon;"); vBox.setAlignment(Pos.CENTER);
+		pane.setStyle("-fx-background-color: lightsalmon;");
+		vBox.setAlignment(Pos.CENTER);
 		hBox.setAlignment(Pos.CENTER);
 		hBox.setSpacing(10);
-		hBox.setPadding(new Insets(15, 12, 15, 12)); vBox.setPadding(new Insets(15, 12, 15, 12));
+		hBox.setPadding(new Insets(15, 12, 15, 12));
+		vBox.setPadding(new Insets(15, 12, 15, 12));
 		startGameBtn.setMinSize(70, 25);
 		exitGameBtn.setMinSize(70,25);
 
 		return new Scene(pane, 500, 500);
+	}
+
+	// Creates the game play screen
+	private Scene gamePlayScreen() {
+		HBox hBox = new HBox();
+		gameLog = new ListView<>();
+		whichPlayer = new Text();
+		whichPlayer.setText("Player One");
+		gameBoard = new GridPane();
+
+		gameBoard.setMinWidth(300);
+		gameBoard.setMaxWidth(400);
+		addGrid(gameBoard);
+
+		gameLog.setEditable(true);
+		gameLog.setMaxSize(400, 200);
+		hBox.setAlignment(Pos.CENTER);
+		hBox.setSpacing(10);
+		hBox.setPadding(new Insets(15, 12, 15, 12));
+		hBox.getChildren().addAll(gameLog, whichPlayer);
+		gameBoard.setAlignment(Pos.CENTER);
+		gamePane.setCenter(gameBoard);
+		gamePane.setBottom(hBox);
+		gamePane.setStyle("-fx-background-color: lightsalmon;");
+
+		return new Scene(gamePane, 700, 700);
+	}
+
+	// Loads the game buttons into the grid pane and into an internal 2d array to be used for game logic
+	private void addGrid(GridPane pane) {
+		for (int x = 0; x < COLUMNS; x++) {
+			for (int y = 0; y < ROWS; y++) {
+				GameButton button = new GameButton(x, y);
+
+				button.setMinSize(50, 50);
+				button.setStyle("-fx-background-color: transparent");
+				button.setStyle("-fx-border-color: black; -fx-border-width: 2px");
+				gameArray[y][x] = button;
+				pane.add(button, x, y);
+			}
+		}
+		pane.setHgap(10);
+		pane.setVgap(10);
 	}
 }
